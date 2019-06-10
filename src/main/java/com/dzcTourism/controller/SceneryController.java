9f00a00351b7c5abecbd750393bc6e2006e656e8
wangzhitong <wztys@hotmail.com>
2019-06-10 17:28:22 +0800

    提交到开发分支

--- src/main/java/com/dzcTourism/controller/SceneryController.java
+++ src/main/java/com/dzcTourism/controller/SceneryController.java
@@ -1,16 +1,17 @@
 package com.dzcTourism.controller;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.util.ClassUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -19,10 +20,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dzcTourism.domain.Msg;
@@ -36,32 +36,65 @@ import com.dzcTourism.util.StrUtils;
  *
  */
 @RequestMapping("/scenery")
-@RestController
+@Controller
 public class SceneryController {
 
 	@Autowired
 	private SceneryService sceneryService;
 	
+	@ResponseBody
 	@RequestMapping(value = "/getAll",method = RequestMethod.GET)
 	public Msg getAll() {
 		List<Scenery> sceneryAll = sceneryService.getAll();
 		return Msg.success().add("data", sceneryAll);
 	}
 	
+	@ResponseBody
 	@RequestMapping(value = "/getSceneryById",method = RequestMethod.GET)
 	public Msg getSceneryById(String id) {
 		Scenery scenery = sceneryService.getSceneryById(id);
 		return Msg.success().add("scenery", scenery);
 	}
 	
+	@ResponseBody
 	@RequestMapping(value = "/delScenery",method = RequestMethod.GET)
 	public Msg deleteScenery(String id) {
 		sceneryService.deleteScenery(id);
 		return	Msg.success();
 	}
 	
+	/*
+	 * @RequestBody 在一个请求方法中只能有一个，ajax请求体只有一个
+	 * ajax请求的写法 contentType:"application/json charset=utf-8",
+	 */
+	@RequestMapping(value = "/save1",method = RequestMethod.POST)
+	public String save(@RequestParam("file") MultipartFile multipartFile,
+		 HttpServletRequest request, HttpServletResponse response,RedirectAttributes model) {
+		//通过file.getInputStream();处理
+		System.out.println(multipartFile.getOriginalFilename());
+		if(multipartFile != null) {
+			String fileName = multipartFile.getOriginalFilename();
+			String filePath = ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static/upload/img";
+			File file = new File(filePath,fileName);
+			if(!file.getParentFile().exists()) {
+				file.getParentFile().mkdirs();
+			}
+			try {
+				multipartFile.transferTo(file);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			String strPath = filePath+"/"+fileName;
+			System.out.println(strPath);
+			model.addAttribute("path", strPath);
+		}
+		return "redirect:save";
+	}
+	
+	
+	@ResponseBody
 	@RequestMapping(value = "/save",method = RequestMethod.POST)
-	public Msg saveScenery(@ModelAttribute String path,@RequestBody @Valid Scenery scenery,BindingResult result) {
+	public Msg saveScenery(@ModelAttribute(value = "path") String path,@RequestBody @Valid Scenery scenery,BindingResult result) {
 		if(result.hasErrors()) {
 			Map<String,Object> map = new HashMap<>();
 			List<FieldError> fieldErrors = result.getFieldErrors();
@@ -70,12 +103,12 @@ public class SceneryController {
 			}
 			return Msg.fail().add("data", map);
 		}else {
-			
+			scenery.setHomePic(path);
 			sceneryService.saveScenery(scenery);
 			return	Msg.success();
 		}
 	}
-	
+	@ResponseBody
 	@RequestMapping(value = "/updateScenery",method = RequestMethod.POST)
 	public Msg updateScenery(@RequestBody Scenery scenery) {
 		if(scenery != null && scenery.getId() != null) {
@@ -97,6 +130,7 @@ public class SceneryController {
 	}
 	
 	//验证
+	@ResponseBody
 	@RequestMapping(value = "/checking",method = RequestMethod.GET)
 	public Msg checking(String title) {
 		boolean checkIsExist = sceneryService.checkIsExist(title);
